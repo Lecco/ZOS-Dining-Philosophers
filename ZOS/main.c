5e5e3dbedfd3ad50e7428cf4cd09f61d705fdcc3
Oldřich Pulkrt <O.Pulkrt@gmail.com>
2013-12-13 11:17:09 +0100

    Initial commit

---
+++ ZOS/main.c
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.c
+ * Author: Oldřich Pulkrt
+ *
+ * Created on 13. prosinec 2013, 10:59
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    return 0;
+}
